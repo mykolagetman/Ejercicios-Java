cf6d5b943d73f29a6f11bd80d95aadbf7d73613b
Javier Luque Rodríguez <52297980+javierluque96@users.noreply.github.com>
2019-10-19 15:33:26 +0200

    Add files via upload

---
+++ Aprende Java/Ejercicios Tema 05/Ejercicicios_Extra/ExtraBucles3.java
@@ -0,0 +1,45 @@
+public class ExtraBucles3 {
+  
+  public static void main (String[] args) {
+    System.out.println("Adivina el número. PISTA: el número está entre el "
+      + "0 y el 10.000.");
+    
+    int num = 1996;
+    int adivina;
+    int mayor = 10000;
+    int menor = 0;
+    boolean adivinado;
+  
+    
+    do { // Incompleto falta poner cual es el numero más cercano al numero a adivinar
+      adivinado = false;
+      
+      System.out.print("\nIntroduce un número: ");
+      adivina = Integer.parseInt(System.console().readLine());
+      
+      if (adivina > num) {
+	if ((mayor - num) > (adivina - num)) {
+	  mayor = adivina;
+	}
+	
+	System.out.println("\n" + adivina + " es más grande que el número a "
+	  + "adivinar.\nNúmero mayor más cercano: " + mayor + "\nNúmero menor "
+	    + "más cercano: " + menor);
+	    
+      } else if (adivina < num) {
+	if ((num - menor) > (num - adivina)) {
+	  menor = adivina;
+	}
+	
+	System.out.println("\n" + adivina + " es más pequeño que el número a "
+	  + "adivinar.\nNúmero mayor más cercano: " + mayor + "\nNúmero menor "
+	    + "más cercano: " + menor);
+	    
+      } else {
+	System.out.println("\n¡¡Enhorabuena, has acertado el número!!");
+	adivinado = true;
+      }
+    } while (adivinado == false);
+  } 
+}
+
